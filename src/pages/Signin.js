223e06873ff7384652c844883648def0538eac2d
SuhanaJabin <suhanajabinpk@gmail.com>
2023-12-30 18:30:22 +0530

    first commit

---
+++ src/pages/Signin.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useNavigate } from "react-router-dom";
+
+function Signin() {
+
+    const navigate=useNavigate();
+    const fn = () =>
+    {
+        navigate("/Signup")
+    }
+  return (
+    <div>
+    <h2>Sign In</h2>
+    <form action="/action_page.php">
+      <div className="flex flex-col">
+        <div>
+          {" "}
+          <label for="fname">Name</label>
+          <input type="text" id="fname" name="fname" value="Name" />
+        </div>
+        <div>
+          {" "}
+          <label for="lname">Email</label>
+          <input type="text" id="lname" name="lname" value="Email" />
+        </div>
+        <div>
+          <label for="lname">Password</label>
+          <input type="password" id="lname" name="lname" value="Password" />
+        </div>
+      </div>
+    </form>
+
+    <button onClick={() => fn()}>Sign Up</button>
+    <button>Login</button>
+  </div>
+  )
+}
+
+export default Signin
